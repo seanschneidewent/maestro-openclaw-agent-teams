8cc7b989d9c9057aefc91784d17010e2412756a6
seanschneidewent <seanschneidewent@gmail.com>
2026-02-25 22:51:59 -0600

    Bump maestro wheel versions to 0.1.1

--- packages/maestro-engine/src/maestro_engine/__init__.py
+++ packages/maestro-engine/src/maestro_engine/__init__.py
@@ -1,3 +1,3 @@
 """Shared Maestro engine primitives."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
--- packages/maestro-solo/src/maestro_solo/__init__.py
+++ packages/maestro-solo/src/maestro_solo/__init__.py
@@ -1,3 +1,3 @@
 """Maestro Solo product package."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
